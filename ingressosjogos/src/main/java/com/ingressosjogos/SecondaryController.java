d583c1fa0380084f7fb01cfa943734cd1a0e1798
Janailson Gomes <janailson.gomes@academico.ifpb.edu.br>
2026-02-05 11:08:56 -0300

    Add files via upload

---
+++ ingressosjogos/src/main/java/com/ingressosjogos/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.ingressosjogos;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
